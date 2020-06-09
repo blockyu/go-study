503986b6294fcc9dff33c30bf5d5f9239d209099
blockyu <blockyu1993@gmail.com>
2020-06-09 09:24:17 +0900

    cmd test

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -15,7 +15,10 @@ limitations under the License.
 */
 package main
 
-import "github.com/blockyu/go-study/cmd"
+import (
+	"github.com/blockyu/cmd/root"
+	"github.com/spf13/cobra/cobra/cmd"
+)
 
 func main() {
 	cmd.Execute()
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module go-study
 go 1.14
 
 require (
-	github.com/spf13/cobra v1.0.0 // indirect
+	github.com/spf13/cobra v1.0.0
 	github.com/spf13/viper v1.7.0 // indirect
 )
